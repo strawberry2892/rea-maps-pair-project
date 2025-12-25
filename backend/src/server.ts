439876c087132b00adac1c68a38da325e67ce2fb
strawberry2892 <dashadymova21@gmail.com>
2025-12-25 19:17:05 +0300

    add base project files

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import placeholder from "./routes/placeholder";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
